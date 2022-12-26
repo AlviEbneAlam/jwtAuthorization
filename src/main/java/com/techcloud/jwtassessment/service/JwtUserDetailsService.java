010df39750f8577a08ee92afb4dc1843ddfce476
210505-ALVI\Alvi <t88.alam@gmail.com>
2022-12-26 17:27:29 +0600

    Second commit

--- src/main/java/com/techcloud/jwtassessment/service/JwtUserDetailsService.java
+++ src/main/java/com/techcloud/jwtassessment/service/JwtUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.techcloud.jwtassessment.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,11 +13,13 @@ import java.util.ArrayList;
 @Service
 @Slf4j
 public class JwtUserDetailsService implements UserDetailsService {
+	@Value("${jwt.username}")
+	private String jwtUsername;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		log.info("Username in loadUserByUsername class is: "+username);
-		if ("Alvi Ibne Alam".equals(username)) {
+		if (jwtUsername.equals(username)) {
 			return new User("Alvi Ibne Alam", "$2a$10$slYQmy",
 					new ArrayList<>());
 		} else {
